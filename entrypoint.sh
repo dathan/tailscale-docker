da6490e6bcac51f1b11a5c78dd12cc0a20274ec5
Dathan Vance Pattishall <dathanvp@gmail.com>
2020-09-01 12:51:24 -0700

    support for a helmfile to install tailscale on k8s

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ mknod /dev/net/tun c 10 200 # TODO write an explination as to why this is needed
 
 # Wait 5s for the daemon to start and then run tailscale up to configure
 echo "Starting Tailscale client"
-/bin/sh -c "sleep 10; /usr/bin/tailscale up" &
+/bin/sh -c "sleep 10; /usr/bin/tailscale up --advertise-routes=${ADVERTISE-ROUTES}" &
 exec /usr/sbin/tailscaled --state=/tailscale/tailscaled.state
